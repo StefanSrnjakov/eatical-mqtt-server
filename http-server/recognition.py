958a00dd5ce931d21e407926d422e9a52412173c
Stefan Srnjakov <stefan.srnjakov@student.um.si>
2023-01-29 12:01:47 +0100

    bugfixes

--- http-server/recognition.py
+++ http-server/recognition.py
@@ -1,10 +1,17 @@
-imgDirPath = "D:\\fakultet\\treta\\prvSem\\proekti\\pora\\backend\\eatical-mqtt-server\\images"
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import numpy as np
+
+imgDirPath = "D:\\fakultet\\treta\\prvSem\\proekti\\pora\\backend\\eatical-mqtt-server\\images\\"
 # add your path here
 def food_recognition(file_name):
-    return {
-        "path": file_name,
-        "topic": "food"
-    }
+    img = mpimg.imread(imgDirPath + file_name)
+    plt.imshow(img, cmap="gray")
+    plt.axis('off')
+    plt.show()
+    return {"works" : True}
+
+
 
 
 def menu_recognition(file_name):
